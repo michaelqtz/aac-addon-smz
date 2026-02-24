9dbdeb6e15195a9f0794c77a728797b0b4cc86e4
michaelqtz <michaelqtgames@gmail.com>
2026-02-24 15:43:17 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -20,6 +20,14 @@ local ZONE_STATES = {
 local zoneCheckTimer = 0
 local zoneCheckTimerRate = 5000
 
+local landForSaleTimer = 0
+local landForSaleTimerRate = 6000
+
+local towerDefMsgsToWrite = {}
+
+
+local smzWindow = nil
+
 
 local function encodeToJson(tbl)
     local function escapeStr(s)
@@ -62,7 +70,7 @@ local function checkZoneStates()
             if zoneInfo.isConflictZone ~= false then 
                 local conflictState = tonumber(zoneInfo.conflictState)
                 infoToWrite.conflictState = conflictState
-                if conflictState >= ZONE_STATES.CONFLICT and conflictState <= ZONE_STATES.WAR then 
+                if conflictState >= 5 then 
                     local remainingTime = zoneInfo.remainTime 
                     infoToWrite.remainingTime = remainingTime
                     -- api.Log:Info("Conflict Zone: " .. zoneInfo.zoneName .. " ID: " .. tostring(i) .. " State: " .. tostring(conflictState) .. " Time Left: " .. tostring(remainingTime))
@@ -76,6 +84,9 @@ local function checkZoneStates()
                 -- api.Log:Info("Local Development Zone: " .. zoneInfo.zoneName .. " ID: " .. tostring(i) .. " Rank: " .. tostring(localDevelopmentRank) .. " Rank Name: " .. tostring(localDevelopmentRankName))
             end 
 
+            -- if i == 22 then 
+            --     api.Log:Info(zoneInfo)
+            -- end 
             
             local jsonString = encodeToJson(infoToWrite)
             jsonString = markStringWithDelimiters(jsonString, "###")
@@ -86,19 +97,65 @@ local function checkZoneStates()
     api.File:Write("sermeatball_zone_states.txt", table.concat(jsonStrings, "\n"))
 end 
 
+local function writeTowerDefMsgs()
+    local jsonStrings = {}
+    for _, msgInfo in ipairs(towerDefMsgsToWrite) do
+        local jsonString = encodeToJson(msgInfo)
+        jsonString = markStringWithDelimiters(jsonString, "###")
+        table.insert(jsonStrings, jsonString)
+    end
+    if #jsonStrings > 0 then 
+        api.File:Write("sermeatball_tower_def_msgs.txt", table.concat(jsonStrings, "\n"))
+        towerDefMsgsToWrite = {}
+    end
+end 
+
+local function checkLandForSale()
+    -- Placeholder for future implementation
+end
+
 local function OnUpdate(dt)
     zoneCheckTimer = zoneCheckTimer + dt
     if zoneCheckTimer >= zoneCheckTimerRate then 
         zoneCheckTimer = 0
         checkZoneStates()
+        writeTowerDefMsgs()
     end
 end 
 
 local function OnLoad()
     local settings = api.GetSettings("sermeatball")
 
-    
-    
+    smzWindow = api.Interface:CreateEmptyWindow("smzWindow", "UIParent")
+    smzWindow:Show(true)
+
+    local smzEvents = {
+        TOWER_DEF_MSG = function(towerDefInfo)
+            local zoneGroup = tostring(towerDefInfo.zoneGroup)
+            local text = string.format("%s%s", towerDefInfo.color, towerDefInfo.msg)
+            local title = string.format("%s%s", towerDefInfo.color, towerDefInfo.titleMsg)
+            local step = tostring(towerDefInfo.step)
+            local iconName = string.format("%s_%s", towerDefInfo.iconKey, towerDefInfo.step)
+            -- api.Log:Info(string.format("Tower Defense Event - Zone Group: %s, Text: %s, Title: %s, Step: %s, Icon: %s", zoneGroup, text, title, step, iconName))
+            -- Store the message to write at 60 second intervals
+            table.insert(towerDefMsgsToWrite, {
+                zoneGroup = zoneGroup,
+                text = text,
+                title = title,
+                step = step,
+                iconName = iconName
+            })
+        end,
+        GRADE_ENCHANT_BROADCAST = function(characterName, resultCode, itemLink, oldGrade, newGrade)
+
+        end,
+    }
+    smzWindow:SetHandler("OnEvent", function(this, event, ...)
+        smzEvents[event](...)
+    end)
+    for eventName, _ in pairs(smzEvents) do
+        smzWindow:RegisterEvent(eventName)
+    end
 
     api.On("UPDATE", OnUpdate)
     api.SaveSettings()
@@ -106,6 +163,7 @@ end
 
 local function OnUnload()
     api.On("UPDATE", function() return end)
+    api.Interface:Free(smzWindow)
 end
 
 addon.OnLoad = OnLoad
